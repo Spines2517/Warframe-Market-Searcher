06708cd0c341765728d55313ea8ae9aebbc0be33
小刺Spines <115948354+Spines2517@users.noreply.github.com>
2026-06-15 10:35:52 +0800

    Refactor WFM_Searcher to use main function
    Refactor code to use main function and access specific worksheet.

--- WFM_Searcher.py
+++ WFM_Searcher.py
@@ -3,30 +3,31 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 
-def test_service_account_access():
+def main():
     creds_json = os.environ.get("GOOGLE_CREDENTIALS")
     if not creds_json:
         print("❌ 沒有讀到 GOOGLE_CREDENTIALS")
         return
 
+    # Secrets 是字串，要轉成 dict
     creds_dict = json.loads(creds_json)
 
-        # 指定正確的 scope
+    # 指定正確的 scope
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    
+
     creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
     gc = gspread.authorize(creds)
 
-
     try:
-        # 替換成你的試算表名稱
-        sh = gc.open("WFM")
+        sh = gc.open("WFM")  # 替換成你的試算表名稱
+        worksheet = sh.worksheet("匯入")
         print("✅ 成功存取試算表:", sh.title)
+        print("✅ 成功存取工作表:", worksheet.title)
     except Exception as e:
         print("❌ 存取失敗:", e)
 
 if __name__ == "__main__":
-    test_service_account_access()
+    main()
